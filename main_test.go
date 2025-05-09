fdeb1da8ddd0fd08cfa0162482a9e2a4aaeb524c
lautaromdelgado <lautaromdelgado@gmail.com>
2025-05-09 01:37:22 -0300

    change: refactorización implementando el paquete testify en el test unitario anterior

--- main_test.go
+++ main_test.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestAddSuccess(t *testing.T) {
+	c := require.New(t)
 	result := Add(20, 2)
 	expected := 22
-	if result != expected {
-		t.Errorf("se esperaba %d, pero se obtuvo %d", expected, result)
-	}
+	c.Equal(expected, result, "se esperaba %d pero se obtuvo %d", expected, result)
 }
